0d858bfa0d6b4140b66edd4b90ca943cb29ccbde
mir592 <miriam5592@gmail.com>
2017-09-20 17:24:30 +0200

    REFACTORIZACION DEFINITIVA FUNCIONA IGUAL QUE ANTERIOR SIN REFACTORIZAR

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ require('./config/expressController')(app);
 const index = require('./routes/index');
 const authController = require('./routes/auth');
 const addController = require('./routes/add');
+const viewQues = require('./routes/answQuest');
 
 app.use('/', index);
 app.use('/', authController);
 app.use('/', addController);
+app.use('/', viewQues);
 
 require('./config/error-handler')(app);
 
