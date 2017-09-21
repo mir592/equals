07d6017b4f2c789f5288f898f2617666b5e3d164
Danigp <danielentj@gmail.com>
2017-09-21 20:12:05 +0200

    merge/ update sin post

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require ("../models/users");
+const User = require("../models/users");
 //const newController = require('../controllers/newController');//
 
 
@@ -8,8 +8,10 @@ router.get('/user/:id/edit', (req, res, next) => {
   const userId = req.params.id;
 
   User.findById(userId)
-  .then(user => res.render('user/edit', { user: user }))
-  .catch(err => next(err));
+    .then(user => res.render('user/edit', {
+      user: user
+    }))
+    .catch(err => next(err));
 });
 
 
@@ -18,17 +20,18 @@ router.post('/user/:id', (req, res, next) => {
   const userId = req.params.id;
 
   const updates = {
-      name: req.body.name,
-      alias: req.body.alias,
-      email: req.body.email,
-      password: req.body.password,
-      pick: req.body.pick,
+    name: req.body.name,
+    alias: req.body.alias,
+    email: req.body.email,
+    password: req.body.password,
+    pick: req.body.pick,
   };
 
-  User.findByIdAndUpdate(userId, updates, (err, user) => {
-    if (err){ return next(err); }
-    return res.redirect('/');
-  });
+  User.findByIdAndUpdate(userId, updates)
+    .save()
+    .then(user => res.redirect('login'))
+    .catch(e => res.render("/user/:id"));
+
 });
 
 module.exports = router;
