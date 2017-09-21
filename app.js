37fb95baf3436ae6ba528b70cacabe4f1e4ff013
Danigp <danielentj@gmail.com>
2017-09-21 19:43:43 +0200

    update usuarios

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const index = require('./routes/index');
 const authController = require('./routes/auth');
 const newController = require('./routes/new');
 const questionsRoutes = require('./routes/questionsList');
+const userRoutes = require('./routes/user');
 
 app.use('/', index);
 app.use('/', authController);
 app.use('/', newController);
 app.use('/', questionsRoutes);
+app.use('/', userRoutes);
 
 require('./config/error-handler')(app);
 
